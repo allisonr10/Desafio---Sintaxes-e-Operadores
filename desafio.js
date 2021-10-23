2b8cac2728302e3c283bcbe0ca7f2531f0628cf4
Allison Rodrigues <allison.ifg@gmail.com>
2021-10-23 17:42:08 -0300

    Finalizado
    Projeto um pouco diferente, mas com o mesmo resultado.

---
+++ desafio.js
@@ -0,0 +1,27 @@
+/*
+Atividade
+Crie uma função que recebe dois números como parâmetros.
+Confira se os números são iguais.
+Confira se a soma dos números é maior que 10 ou menor que 20.
+Retorne uma string dizendo "Os números num1 e num2 não/são iguais. Sua soma é soma, que é maior/menor que 10 e maior/menor que 20". 
+*/
+
+function comparar(num1,num2){
+    let soma = 0
+    soma = num1 + num2
+    if(num1 === num2){
+       console.log(`Os números ${num1} e ${num2} são iguais`)
+    }else{
+        console.log(`Os números ${num1} e ${num2} não são iguais`)
+    }
+    if(soma < 10){
+        console.log(`Sua soma é ${soma}, que é menor que 10 e menor que 20`)
+    }else if(soma > 10 && soma < 20){
+        console.log(`Sua soma é ${soma}, que é maior que 10 e menor que 20`)
+    }else if(soma === 20){
+        console.log(`Sua soma é ${soma}, que é maior que 10`)
+    }
+    
+}
+
+comparar(10,10)
